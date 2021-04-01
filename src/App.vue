f4a55ee1ae02de4763b6dbe271486e044feee7c5
Akshat Shukla <shuklaakshatascom@gmail.com>
2021-04-01 14:15:22 +0530

    Added search button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <main>
       <div class="search-div">
         <input type="text" class="search-location" placeholder="Search..." v-model="search_query" @keypress.enter="fetchWeather">
+        <button class="search-btn" @click="fetchWeather">Go</button>
       </div>
       <div class="location-div">
         <div class="location">{{ weather.city }}, {{ weather.country }}</div>
@@ -43,6 +44,8 @@ export default {
       this.weather.city = json.name;
       this.weather.country = json.sys.country;
       this.weather.date = this.formatDate();
+
+      // if (this.weather.temp >= 25) document.querySelector("#app").style.background = "url('./assets/warm-bg.jpg')";
     },
     formatDate() {
       const months = ["January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December"]
@@ -90,8 +93,7 @@ main {
 }
 
 .search-location {
-  display: block;
-  width: 100%;
+  width: 75%;
   padding: .5rem;
 
   appearance: none;
@@ -108,6 +110,28 @@ main {
 .search-location:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.search-btn {
+  width: 20%;
+  padding: .5rem;
+  margin-left: 3%;
+
+  appearance: none;
+  outline: none;
+  border: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-btn:hover {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 16px 0px 16px 0px;
 }
 
